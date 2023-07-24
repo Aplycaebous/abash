4dc866265559864ad1c636c7c662eac954fa65d2
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-07-25 00:08:50 +0600

    All Changes Fetched

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:abash/topbar.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+//
+// void main() {
+//   runApp(const RentalDetails());
+// }
 
 void main() {
-  runApp(const RentalDetails());
+  //
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => const RentalDetails(),
+  ));
 }
-
