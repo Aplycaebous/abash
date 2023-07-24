a4f528b795ac4411e0d564e7a62454f641ab65cf
curryfizz <nawsheen141@gmail.com>
2023-07-24 23:10:22 +0600

    device preview enbaled

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:abash/topbar.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+//
+// void main() {
+//   runApp(const RentalDetails());
+// }
 
 void main() {
-  runApp(const RentalDetails());
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => const RentalDetails(),
+  ));
 }
-
