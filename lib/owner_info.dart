b044c464a44a349f42df0e22eea4752d4465c1dc
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-07-25 16:37:19 +0600

    woohoooo owner info fixedddddd

--- lib/owner_info.dart
+++ lib/owner_info.dart
@@ -15,17 +15,17 @@ class OwnerInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-        decoration: BoxDecoration(
-          color: Colors.white, // Set white background color
-        ),
-        child: AppBar(
-          backgroundColor: Colors.white,
-          title: Row(
+      padding: const EdgeInsets.symmetric(
+          vertical: 10), // Add padding for the entire widget
+      color: Colors.white, // Set white background color
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
             children: [
               CircleAvatar(
                 radius: 24,
-                backgroundImage: NetworkImage("assets/images/image 7.png"),
+                backgroundImage: NetworkImage("assets/images/image 8.png"),
               ),
               const SizedBox(width: 10),
               Column(
@@ -33,47 +33,46 @@ class OwnerInfo extends StatelessWidget {
                 children: [
                   Text(
                     userName,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                   Text(
                     isOwner ? "Owner (You)" : "Owner",
-                    style: TextStyle(fontSize: 14),
+                    style: const TextStyle(fontSize: 14),
                   ),
                 ],
               ),
             ],
           ),
-          actions: [
-            if (!isOwner)
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 4),
-                child: Container(
-                  padding: EdgeInsets.all(4), // Add padding around the IconButton
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(18),
-                    gradient: const LinearGradient(
-                      colors: [Color(0xFF5FB4E5), Color(0xFF0A8ED9)],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                      stops: [0.0, 1.0],
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color(0xFFB2B2B2).withOpacity(0.33),
-                        offset: const Offset(0, 10),
-                        blurRadius: 30,
-                        spreadRadius: 0,
-                      ),
-                    ],
-                  ),
-                  child: IconButton(
-                    icon: const Icon(Icons.call_outlined),
-                    onPressed: onCallPressed,
-                    color: Colors.white,
-                  ),
+          if (!isOwner)
+            Container(
+              padding:
+                  const EdgeInsets.all(4), // Add padding around the IconButton
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                gradient: const LinearGradient(
+                  colors: [Color(0xFF5FB4E5), Color(0xFF0A8ED9)],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  stops: [0.0, 1.0],
                 ),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0xFFB2B2B2).withOpacity(0.33),
+                    offset: const Offset(0, 10),
+                    blurRadius: 30,
+                    spreadRadius: 0,
+                  ),
+                ],
               ),
-          ],
-        ));
+              child: IconButton(
+                icon: const Icon(Icons.call_outlined),
+                onPressed: onCallPressed,
+                color: Colors.white,
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
