1b9b95c9aefc36dde5e3405f86cb4f29377de9bd
curryfizz <nawsheen141@gmail.com>
2023-08-07 20:19:15 +0600

    verification badge created

---
+++ lib/profile/verification_badge.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class VerificationBadge extends StatelessWidget {
+  final bool verified;
+
+  VerificationBadge({this.verified = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * (21.0 / 800.0),
+      width: MediaQuery.of(context).size.width * (90.0 / 360.0),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        decoration: BoxDecoration(
+          color: verified ? Colors.green : Colors.grey,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              verified ? Icons.check : Icons.close,
+              color: Colors.white,
+              size: 10,
+            ),
+            SizedBox(width: 4),
+            Text(
+              verified ? 'Verified User' : 'Not Verified',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 10,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
