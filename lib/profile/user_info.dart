6880e8d05503249cc15b37307666d0ef13c972ab
curryfizz <nawsheen141@gmail.com>
2023-08-13 20:39:11 +0600

    user info works perfectly

--- lib/profile/user_info.dart
+++ lib/profile/user_info.dart
@@ -1,6 +1,8 @@
 import 'package:abash/profile/user_profile_avatar.dart';
 import 'package:abash/profile/verification_badge.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class UserInfoCard extends StatelessWidget {
   final Map<String, dynamic> userInfo;
@@ -16,60 +18,85 @@ class UserInfoCard extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.all(
             MediaQuery.of(context).size.width * (16.0 / 360.0)), // Add padding of 16
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: [
-            UserProfileAvatar(imageUrl: userInfo['profilePicture']),
-            SizedBox(width: 15), // Add spacing between avatar and text
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                if(!userInfo['verified'])...[
-                SizedBox(height: 20)],
-                Text(
-                  userInfo['name'], // Replace with actual user's name
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 10),
-                VerificationBadge(verified: userInfo['verified']),
-                if (!userInfo['verified']) ...[
-                  SizedBox(height: 8),
-                  Container(
-                    constraints: BoxConstraints(maxWidth: 200),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text:
-                                "Without verification you cannot rent or add properties. ",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.black, // Keep the text color black
-                                ),
-                              ),
-                              TextSpan(
-                                text: "Verify now",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xFF0A8ED9), // Make the text blue
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                UserProfileAvatar(imageUrl: userInfo['profilePicture']),
+                SizedBox(width: 15), // Add spacing between avatar and text
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (!userInfo['verified']) ...[
+                      SizedBox(height: 20)
+                    ],
+                    Text(
+                      userInfo['name'], // Replace with actual user's name
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w900,
+                      ),
                     ),
-                  ),
-                ], // <- Missing opening brace {
+                    SizedBox(height: 10),
+                    VerificationBadge(verified: userInfo['verified']),
+                    if (!userInfo['verified']) ...[
+                      SizedBox(height: 8),
+                      Container(
+                        constraints: BoxConstraints(maxWidth: 200),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text:
+                                    "Without verification you cannot rent or add properties. ",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.black, // Keep the text color black
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: "Verify now",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFF0A8ED9), // Make the text blue
+                                    ),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        // Handle Verify now button click
+                                      },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
               ],
+            ),
+            Positioned(
+              top: 20,
+              right: 0,
+              child: IconButton(
+                onPressed: () {
+                  // Handle edit button action here
+                },
+                icon: SvgPicture.asset(
+                  "assets/icons/edit_square.svg",
+                  // Replace with the actual path
+                  width: 18,
+                  height: 18,
+                ),
+              ),
             ),
           ],
         ),
@@ -77,3 +104,4 @@ class UserInfoCard extends StatelessWidget {
     );
   }
 }
+
