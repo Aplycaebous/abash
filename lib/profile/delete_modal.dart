658c2ea5d26d8b8290ab7def6a73b5365797181c
curryfizz <nawsheen141@gmail.com>
2023-08-13 23:05:00 +0600

    delete modal completed

--- lib/profile/delete_modal.dart
+++ lib/profile/delete_modal.dart
@@ -1,37 +1,120 @@
 import 'package:flutter/material.dart';
 
-//complete this!!
 class DeleteModal extends StatelessWidget {
-  const DeleteModal({super.key});
+  const DeleteModal({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-// <<<<<<< HEAD
-//       title: Text("Delete Option"),
-//       content: Text("This is the delete modal content."),
-// =======
-      title: const Center(
-          child: Text("Remove Property?",style: TextStyle(color: Colors.black, fontSize: 18))
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-// <<<<<<< HEAD
-//           child: Text("Close"),
-//         ),
-// =======
-          child: const Text("Yes"),
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      contentPadding: EdgeInsets.zero,
+      content: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12.0),
+          child: Container(
+            height: 124,
+            width: 272,
+            color: Colors.white,
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Text(
+                      "Delete Property?",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(child: SizedBox()),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [Color(0xFF5FB4E5), Color(0xFF0A8ED9)],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                              ),
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                // Perform delete action here
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.transparent,
+                                elevation: 0,
+                                padding: EdgeInsets.zero,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(6.0),
+                                ),
+                              ),
+                              child: Container(
+                                width: double.infinity,
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "Yes",
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 12),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              elevation: 0,
+                              padding: EdgeInsets.zero,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6.0),
+                                side: BorderSide(
+                                  color: Colors.grey.shade300,
+                                ),
+                              ),
+                            ),
+                            child: Container(
+                              width: double.infinity,
+                              alignment: Alignment.center,
+                              child: Text(
+                                "No",
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Color(0xFF0A8ED9),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text("No"),
-        )
-      ]
+      ),
     );
   }
 }
