14938197f28b8ea2f93c3130ef6cf941df0d1e15
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-09-15 22:59:18 +0600

    minor changes

--- lib/chatbox/chatbox.dart
+++ lib/chatbox/chatbox.dart
@@ -29,7 +29,6 @@ class Chatbox extends StatelessWidget {
                       ReceivedMessage(text: "Hello, I'm Khalid. Here some pictures."),
                       const ReceivedImage(imageUrl: "assets/images/chat image.png"),
                       SentMessage(text: "Show me other options"),
-                      const SentImage(imageUrl: "assets/images/chat image.png"),
                       ReceivedMessage(text: "That's all I can show"),
                       // Add more messages here if needed
                     ],
