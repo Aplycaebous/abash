c76a438efdbd81efc27bce9b2c510c4976f30fcb
curryfizz <nawsheen141@gmail.com>
2023-09-16 00:39:28 +0200

    chatbox topbar is dynamic in height and space between user avatar and back button icon

--- lib/chatbox/chatbox-top-bar.dart
+++ lib/chatbox/chatbox-top-bar.dart
@@ -1,23 +1,33 @@
+import 'dart:math';
+
 import 'package:abash/chatbox/chat-user-avatar.dart';
 import 'package:flutter/material.dart';
 
 class ChatBoxTopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenIconSize = MediaQuery.sizeOf(context).height * 20 / 800;
+    final sizeBetweenBackButtonAndIcon = min(MediaQuery.sizeOf(context).height,
+            MediaQuery.sizeOf(context).width) *
+        0.02;
     return AppBar(
-      leading: null,
-      title: Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-        IconButton(
-          onPressed: () => {
-            //go back
-          },
-          icon: const Icon(Icons.arrow_back),
-          iconSize: 20,
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+              onPressed: () => {
+                //go back
+              },
+              icon: const Icon(Icons.arrow_back),
+              iconSize: screenIconSize,
+            ),
+            SizedBox(width: sizeBetweenBackButtonAndIcon),
+            ChatUserAvatar(),
+          ],
         ),
-        SizedBox(width: 36-20),
-        ChatUserAvatar(),
-      ]),
-      backgroundColor: Colors.pink,
-    );
+        backgroundColor: Colors.white,
+        // shadowColor: Colors.black12,
+        elevation: 0,
+        scrolledUnderElevation: 0);
   }
 }
