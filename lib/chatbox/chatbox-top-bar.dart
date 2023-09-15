917dc82f3ad41e59cdc2e689f18a7595920e8d9a
curryfizz <nawsheen141@gmail.com>
2023-09-16 01:08:43 +0200

    shadow under top bar fixed

--- lib/chatbox/chatbox-top-bar.dart
+++ lib/chatbox/chatbox-top-bar.dart
@@ -1,33 +1,55 @@
 import 'dart:math';
-
 import 'package:abash/chatbox/chat-user-avatar.dart';
 import 'package:flutter/material.dart';
 
 class ChatBoxTopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final screenIconSize = MediaQuery.sizeOf(context).height * 20 / 800;
-    final sizeBetweenBackButtonAndIcon = min(MediaQuery.sizeOf(context).height,
-            MediaQuery.sizeOf(context).width) *
+    final screenIconSize = MediaQuery.of(context).size.height * 20 / 800;
+    final sizeBetweenBackButtonAndIcon = min(
+          MediaQuery.of(context).size.height,
+          MediaQuery.of(context).size.width,
+        ) *
         0.02;
-    return AppBar(
-        title: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            IconButton(
-              onPressed: () => {
-                //go back
-              },
-              icon: const Icon(Icons.arrow_back),
-              iconSize: screenIconSize,
-            ),
-            SizedBox(width: sizeBetweenBackButtonAndIcon),
-            ChatUserAvatar(),
-          ],
+
+    final shadowColor = Color(0xB2B2B2); // Custom shadow color without opacity
+    final userInfoAndBackButtonWidth =
+        MediaQuery.of(context).size.width * 247 / 800;
+
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: shadowColor.withOpacity(0.2), // Set opacity separately
+            spreadRadius: 0,
+            blurRadius: 30,
+            offset: Offset(0, 10),
+          )
+        ],
+      ),
+      child: AppBar(
+        title: SizedBox(
+          width: userInfoAndBackButtonWidth,
+
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                onPressed: () {
+                  // Go back
+                },
+                icon: const Icon(Icons.arrow_back),
+                iconSize: screenIconSize,
+              ),
+              SizedBox(width: sizeBetweenBackButtonAndIcon),
+              ChatUserAvatar(),
+            ],
+          ),
         ),
         backgroundColor: Colors.white,
-        // shadowColor: Colors.black12,
         elevation: 0,
-        scrolledUnderElevation: 0);
+        scrolledUnderElevation: 0,
+      ),
+    );
   }
 }
