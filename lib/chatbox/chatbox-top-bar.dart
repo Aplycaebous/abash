4cca436e0ad7f57c84c292a131d6f779600e7968
curryfizz <nawsheen141@gmail.com>
2023-09-15 23:36:50 +0200

    top/app bar shows user avatar and back button

--- lib/chatbox/chatbox-top-bar.dart
+++ lib/chatbox/chatbox-top-bar.dart
@@ -1,20 +1,23 @@
+import 'package:abash/chatbox/chat-user-avatar.dart';
 import 'package:flutter/material.dart';
 
-class ChatBoxTopBar extends StatelessWidget implements PreferredSizeWidget {
+class ChatBoxTopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text("231231"),
-      leading: IconButton(
-        onPressed: () => {
-          //go back
-        },
-        icon: const Icon(Icons.arrow_back),
-      ),
+      leading: null,
+      title: Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+        IconButton(
+          onPressed: () => {
+            //go back
+          },
+          icon: const Icon(Icons.arrow_back),
+          iconSize: 20,
+        ),
+        SizedBox(width: 36-20),
+        ChatUserAvatar(),
+      ]),
       backgroundColor: Colors.pink,
     );
   }
-
-  @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
