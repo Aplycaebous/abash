06c7c7042f4b1330e3ddce02aef17578510375dd
curryfizz <nawsheen141@gmail.com>
2023-09-16 03:01:42 +0200

    fixed height issues

--- lib/chatbox/chatbox.dart
+++ lib/chatbox/chatbox.dart
@@ -11,52 +11,48 @@ class Chatbox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final appBarHeight = MediaQuery.sizeOf(context).height * 60 / 800;
     return SafeArea(
-        child: Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(appBarHeight),
-        child: ChatBoxTopBar(),
-      ),
-      body: Column(
-        children: <Widget>[
-          // Scrollable space in the middle with background color
-          Expanded(
-            child: Container(
-              color: const Color(0xFFF9F9F9), // Background color
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding:
-                      const EdgeInsets.only(left: 16.0, right: 16.0, top: 20.0),
-                  // Add horizontal padding
-                  child: Column(
-                    children: <Widget>[
-                      Timestamp(dateTime: DateTime.now()),
-                      ReceivedMessage(
-                          text: "Hello, I'm Khalid. Here some pictures."),
-                      const ReceivedImage(
-                          imageUrl: "assets/images/chat image.png"),
-                      SentMessage(text: "Show me other options"),
-                      const SentImage(
-                          imageUrl: "assets/images/chat image 2.png"),
-                      ReceivedMessage(text: "That's all I can show"),
-                      // Add more messages here if needed
-                    ],
+      child: Scaffold(
+        appBar: ChatBoxTopBar(),
+        body: Column(
+          children: <Widget>[
+            // Scrollable space in the middle with background color
+            Expanded(
+              child: Container(
+                color: const Color(0xFFF9F9F9), // Background color
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 20.0, bottom: 16.0),
+                    // Add horizontal padding and bottom padding
+                    child: Column(
+                      children: <Widget>[
+                        Timestamp(dateTime: DateTime.now()),
+                        ReceivedMessage(
+                            text: "Hello, I'm Khalid. Here are some pictures."),
+                        const ReceivedImage(
+                            imageUrl: "assets/images/chat image.png"),
+                        SentMessage(text: "Show me other options"),
+                        const SentImage(
+                            imageUrl: "assets/images/chat image 2.png"),
+                        ReceivedMessage(text: "That's all I can show"),
+                        // Add more messages here if needed
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
+          ],
+        ),
+        // Fixed bottom app bar
+        bottomNavigationBar: BottomAppBar(
+          // Your bottom app bar content here
+          child: Container(
+            height: 60.0, // Adjust the height as needed
+            color: Colors.blue, // Customize the color of your bottom app bar
           ),
-        ],
-      ),
-      // Fixed bottom app bar
-      bottomNavigationBar: BottomAppBar(
-        // Your bottom app bar content here
-        child: Container(
-          height: 60.0, // Adjust the height as needed
-          color: Colors.blue, // Customize the color of your bottom app bar
         ),
       ),
-    ));
+    );
   }
 }
